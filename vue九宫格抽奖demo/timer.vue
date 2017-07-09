fbaedcf725c03b0175fd89e8b5d8d4f59a3230c5
rianran1993 <rianran1993@163.com>
2017-07-09 15:38:49 +0800

    九宫格抽奖demo，奖品播报，抽中奖品后将获奖信息加入奖品播报末尾

---
+++ vue九宫格抽奖demo/timer.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>        
+        <div id="info">
+            <swiper :options="swiperOption" ref="mySwiper">
+                <swiper-slide v-for="n in 5">{{'恭喜xxxx获得拖鞋一双'+n}}</swiper-slide>
+            </swiper>
+        </div>
+    </div>  
+
+</template>
+
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+    name: 'timer',
+    components: {
+        swiper,
+        swiperSlide
+    },
+    data () {
+        return {
+            swiperOption: {
+                direction : 'vertical',
+                loop : true,
+                autoplayDisableOnInteraction : false,
+                autoplay: 500,
+                height:20
+            }
+        }
+    },
+    methods: {
+        appendSlide(str) {
+            this.$refs.mySwiper.swiper.appendSlide(str);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #info {
+        height: 20px;
+        overflow: hidden;
+    }
+    
+</style>
--- vue九宫格抽奖demo/turntable.vue
+++ vue九宫格抽奖demo/turntable.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="container">
-        <div v-for="n in 9" class="tab" @click="start(n)" :class="{current: n==current}">{{n==5?'开始':n}}</div >
-    </div>
+    <div>        
+        <div class="container">
+            <div v-for="n in 9" class="tab" @click="start(n)" :class="{current: n==current}">{{n==5?'开始':n}}</div >
+        </div>
+        <timers ref="timers"></timers>
+    </div>  
+
 </template>
 
 <script>
 const arr = [1,2,3,6,9,8,7,4];
 let timer, timer1;
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import timers from './timer'
 export default {
     name: 'bookRank',
+    components: {
+        timers
+    },
     data () {
         return {
             current: 1,
@@ -30,8 +39,11 @@ export default {
                     } else {
                         this.current = arr[arr.indexOf(this.current)+1];
                     }
-                    if(speed >= 350) {
-                        if(this.current == target) clearInterval(timer);
+                    if(speed >= 310) {
+                        if(this.current == target) {
+                            clearInterval(timer);
+                            this.$refs.timers.appendSlide('<div class="swiper-slide">这是一个新的slide</div>');
+                        }
                         timer1 = null;  // 清空，否则第二次抽奖时，会一直保持最大速度运行
                         this.isRuning = false;   
                     }else if(speed <= 50) {
@@ -62,12 +74,16 @@ export default {
 </script>
 
 <style scoped>
+    #info {
+        height: 20px;
+        overflow: hidden;
+    }
     .container {
         width: 800px;
         height: 800px;
         display: flex;
         flex-flow: row wrap;
-        background-color: black;
+        background-color: yellow;
     }
     .tab {
         width: 200px;
